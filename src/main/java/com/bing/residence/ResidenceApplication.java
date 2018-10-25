81b31925542affec66c60c246d7d67286e5933a1
bing wu <bing0927w@163.com>
2018-10-25 14:27:57 +0800

    Initial commit

---
+++ src/main/java/com/bing/residence/ResidenceApplication.java
@@ -0,0 +1,12 @@
+package com.bing.residence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidenceApplication.class, args);
+	}
+}
